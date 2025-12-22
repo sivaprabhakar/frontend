5969717adeda1a8504275235540eac3aec4a1402
sivaprabhakaran <sivaprabhakaran.94@gmail.com>
2025-12-22 18:56:48 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Default route → Register */}
+        <Route path="/" element={<Navigate to="/register" />} />
+
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+
+        {/* Catch all unknown routes */}
+        <Route path="*" element={<Navigate to="/register" />} />
+
       </Routes>
     </BrowserRouter>
   );
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8001/api"
+  baseURL: "https://usersbackend-34t9.onrender.com/api"
 });
 
+// Attach token from localStorage
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
